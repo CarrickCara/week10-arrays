ea8c1ca2d53aa01b88404e862d46e225c99f1179
CarrickCara <116729421+CarrickCara@users.noreply.github.com>
2022-11-30 17:38:11 +0000

    create new files
    Add index.html plus script.css

---
+++ script.js
@@ -0,0 +1,7 @@
+function go(){
+    let randomNumbers=[];
+        for( i=0 ; i  < 5 ; i++){
+        randomNumbers[i]=Math.floor(Math.random() * 10) + 1;
+        alert(randomNumbers);
+        }
+    }
